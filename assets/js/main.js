74262d73c11e98fabc6c1c70b616b352843527a3
MelodiousOL <a2005062550322@gmail.com>
2025-10-02 01:12:10 +0800

    update 114.10.02

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,11 +148,12 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100, /*打字速度*/
+      typeSpeed: 70, /*打字速度*/
       backSpeed: 50, /*刪除速度*/
-      backDelay: 2000 /*打完字串後等待幾秒退回*/
+      backDelay: 1500 /*打完字串後等待幾秒退回*/
     });
   }
+
 
   /**
    * Skills animation
